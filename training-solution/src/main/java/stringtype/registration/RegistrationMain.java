3750737dedf73ffd9224b95b8c0fec5d829a0e15
hadikp <hp20001025@gmail.com>
2021-10-21 19:46:34 +0200

    UserValidator commit

--- training-solution/src/main/java/stringtype/registration/RegistrationMain.java
+++ training-solution/src/main/java/stringtype/registration/RegistrationMain.java
@@ -6,9 +6,15 @@ public class RegistrationMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         UserValidator userValid = new UserValidator();
-        System.out.println("Username please!");
-        String userName = scanner.nextLine();
-        userValid.isValidUsername(userName);
-        System.out.println(userName);
+        //System.out.println("Username please!");
+        //String userName = scanner.nextLine();
+        //System.out.println("Username is good: " + userValid.isValidUsername(userName));
+
+        System.out.println("Password please!");
+        String pass1 = scanner.nextLine();
+        System.out.println("Password please again!");
+        String pass2 = scanner.nextLine();
+        System.out.println("A password felvétele sikeres: " + userValid.isValidPassword(pass1, pass2));
+
     }
 }
--- training-solution/src/main/java/stringtype/registration/UserValidator.java
+++ training-solution/src/main/java/stringtype/registration/UserValidator.java
@@ -1,11 +1,27 @@
 package stringtype.registration;
 
 public class UserValidator {
-    public String  isValidUsername(String userName) {
+    public boolean  isValidUsername(String userName) {
         if(userName.equals("")) {
             System.out.println("A felhasználónév megadása kötelező!");
+            return false;
         }
-        return userName;
+        return true;
+    }
 
+    public boolean isValidPassword(String password1, String password2) {
+        if(password1.length() >= 8) {
+            System.out.println("A jelszó 8 karakter hosszú");
+            if(password1.equals(password2)) {
+                System.out.println("A két jelszó megegyezik!");
+                return true;
+            }else {
+                System.out.println("A két jelszó nem egyezik meg!");
+                return false;
+            }
+        }else {
+            System.out.println("A jelszó nincs 8 karakter");
+            return false;
+        }
     }
 }
