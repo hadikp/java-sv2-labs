e256247dd489c303c26312570762e075ee6b24ea
hadikp <hp20001025@gmail.com>
2021-10-21 17:24:24 +0200

    Java API

---
+++ training-solution/src/main/java/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        String msn = "hello world";
+        System.out.println(msn.toUpperCase());
+    }
+}
