729484b8c9e36cb492d493378dda75f3d8920a7f
hadikp <hp20001025@gmail.com>
2021-10-22 11:21:54 +0200

    Négyszögmátrix

---
+++ training-solution/src/main/java/arrayofarrays/Rectangle.java
@@ -0,0 +1,28 @@
+package arrayofarrays;
+
+public class Rectangle {
+    public void rectangularMatrix(int size) {
+        int[][] a = new int[size][];
+        for (int i = size; i > 0; i--) {
+            a[size-i] = new int[size];
+        }
+
+        for(int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                a[j][i] = j;
+            }
+        }
+        //kiíratás
+        for(int i[]: a) {
+            for(int j: i) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Rectangle tetra = new Rectangle();
+        tetra.rectangularMatrix(4);
+    }
+}
