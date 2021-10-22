dedadd38e8a2e5d3bd646a7e64098effea2a2a03
hadikp <hp20001025@gmail.com>
2021-10-22 18:04:10 +0200

    Tömbök kezelése

--- training-solution/src/main/java/arrays/ArraysMain.java
+++ training-solution/src/main/java/arrays/ArraysMain.java
@@ -3,13 +3,39 @@ package arrays;
 import java.util.Arrays;
 
 public class ArraysMain {
-    String numberOfDaysAsString() {
+    public String numberOfDaysAsString() {
         int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 31, 31};
         return Arrays.toString(numberOfDays);
     }
 
+    public String multiplicationTableAsString(int size) {
+        int[][] table = new int[size][size];
+        return Arrays.deepToString(table);
+    }
+
+    public boolean sameTempValues(double[] day, double[] anotherDay) {
+        return Arrays.equals(day, anotherDay);
+    }
+
+    public  boolean wonLottery(int[] lot, int[] win) {
+        Arrays.sort(lot);
+        Arrays.sort(win);
+        return Arrays.equals(lot, win);
+
+    }
+
+
     public static void main(String[] args) {
         ArraysMain numOfDay = new ArraysMain();
         System.out.println(numOfDay.numberOfDaysAsString());
+        System.out.println(numOfDay.multiplicationTableAsString(4));
+
+        double[] day1 = {22, 23, 34, 40};
+        double[] day2 = {22, 23, 34, 40};
+        System.out.println("A két nap azonos hőmérsékleti értékeket tartalmaz: " + numOfDay.sameTempValues(day1, day2));
+
+        int[] lot = {11, 33, 89, 92, 67};
+        int[] win = {33, 11, 89, 92, 67};
+        System.out.println("A kihúzott és megtett számok azonosak: " + numOfDay.wonLottery(lot, win));
     }
 }
