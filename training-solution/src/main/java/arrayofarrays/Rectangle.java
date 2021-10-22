5a99e445fc242fb74d8659bb939d718c3fdf1167
hadikp <hp20001025@gmail.com>
2021-10-22 11:53:46 +0200

    Négyszögmátrix, attribútum refractor

--- training-solution/src/main/java/arrayofarrays/Rectangle.java
+++ training-solution/src/main/java/arrayofarrays/Rectangle.java
@@ -2,18 +2,18 @@ package arrayofarrays;
 
 public class Rectangle {
     public void rectangularMatrix(int size) {
-        int[][] a = new int[size][size];
+        int[][] rect = new int[size][size];
         /*for (int i = size; i > 0; i--) {
-            a[size-i] = new int[size];
+            rect[size-i] = new int[size];
         }*/
 
         for(int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                a[j][i] = j;
+                rect[j][i] = j;
             }
         }
         //kiíratás
-        for(int i[]: a) {
+        for(int i[]: rect) {
             for(int j: i) {
                 System.out.print(j);
             }
