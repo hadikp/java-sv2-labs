f58e3ecfb5c2298da7f07949005004c0f33bd241
hadikp <hp20001025@gmail.com>
2021-10-22 13:04:28 +0200

    Tömbök tömbjének kiírása, első ciklus javítva

--- training-solution/src/main/java/arrayofarrays/ArrayOfArrays.java
+++ training-solution/src/main/java/arrayofarrays/ArrayOfArrays.java
@@ -2,8 +2,8 @@ package arrayofarrays;
 
 public class ArrayOfArrays {
     public void  printArrayOfArrays(int[][] a) {
-        for (int i = 0; i < a[i].length; i++) {
-            for(int j = 0; j < a[j].length; j++) {
+        for (int i = 0; i < a.length; i++) {
+            for(int j = 0; j < a[i].length; j++) {
                 System.out.print(a[i][j] + " ");
             }
             System.out.println();
