fa24f5b9bc935a5483f7228c98faacce02a8f04c
hadikp <hp20001025@gmail.com>
2021-10-22 11:44:17 +0200

    Négyszögmátrix, tömb feltöltés javítva

--- training-solution/src/main/java/arrayofarrays/Rectangle.java
+++ training-solution/src/main/java/arrayofarrays/Rectangle.java
@@ -2,10 +2,10 @@ package arrayofarrays;
 
 public class Rectangle {
     public void rectangularMatrix(int size) {
-        int[][] a = new int[size][];
-        for (int i = size; i > 0; i--) {
+        int[][] a = new int[size][size];
+        /*for (int i = size; i > 0; i--) {
             a[size-i] = new int[size];
-        }
+        }*/
 
         for(int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
