b36a57d7c6cb43915c2f3f60fd49afb5697d29fd
hadikp <hp20001025@gmail.com>
2021-10-23 12:09:05 +0200

    Köszönj

---
+++ training-solution/src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,13 @@
+package finalmodifier;
+
+public class Gentleman {
+    public static final String MESSAGE_PREFIX = "Hello";
+    public String sayHello(String name) {
+        return MESSAGE_PREFIX + " " + name;
+    }
+
+    public static void main(String[] args) {
+        Gentleman man = new Gentleman();
+        System.out.println(man.sayHello("Péter"));
+    }
+}
