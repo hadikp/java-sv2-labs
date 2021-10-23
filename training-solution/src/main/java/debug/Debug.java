352c134cd36d77891b0717a8c004608eb1f28a70
hadikp <hp20001025@gmail.com>
2021-10-23 17:34:57 +0200

    Rosszul megírt utasítások

---
+++ training-solution/src/main/java/debug/Debug.java
@@ -0,0 +1,26 @@
+package debug;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Debug {
+    public static void main(String[] args) {
+        List<String> fruits = Arrays.asList("alma", "körte", "barack", "szilva");
+
+        String peach = fruits.get(2);
+        fruits.set(1, peach);
+        for (int i = 0; i < fruits.size(); i++) {
+            String fruit = fruits.get(i);
+        }
+        System.out.println(fruits.get(1).substring(0, 2));
+
+        int[] numbers = {3, 7, -2, 1, -4, 5};
+
+        numbers[2] = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i]++;
+            numbers[i]++;
+        }
+        System.out.println(numbers[2]);
+    }
+}
