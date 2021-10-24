c3772e808d6845d28b30913ad72e3ec880925a64
hadikp <hp20001025@gmail.com>
2021-10-24 10:47:12 +0200

    kiegészítés

--- training-solution/src/main/java/objects/ObjectsMain.java
+++ training-solution/src/main/java/objects/ObjectsMain.java
@@ -1,5 +1,9 @@
 package objects;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ObjectsMain {
     public static void main(String[] args) {
         System.out.println(new Book());
@@ -14,5 +18,25 @@ public class ObjectsMain {
         System.out.println(book);
         Book anotherBook = new Book();
         System.out.println(book == anotherBook);
+
+
+        Book[] Books = {book, book2, anotherBook};
+        for (Book bk: Books) {
+            System.out.println(bk);
+        }
+        System.out.println();
+        List<Book> bs = Arrays.asList(book, book2, anotherBook);
+        for (Book bk: bs) {
+            System.out.println(bk);
+        }
+
+        System.out.println();
+        List<Book> eBook = new ArrayList<>();
+        eBook.add(book);
+        eBook.add(book2);
+        eBook.add(anotherBook);
+        for (Book bk: eBook) {
+            System.out.println(bk);
+        }
     }
 }
