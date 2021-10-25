8967f4446b80776eb4737bd850ad0bcffd9b9bc3
hadikp <hp20001025@gmail.com>
2021-10-25 19:52:13 +0200

    Literálok kiegészítés

--- training-solution/src/main/java/literals/literalsMain.java
+++ training-solution/src/main/java/literals/literalsMain.java
@@ -17,7 +17,8 @@ public class literalsMain {
         String word = "title";
         System.out.println(word.toUpperCase());
         System.out.println(Integer.toBinaryString(3)); //Integer konvertálása binárissá
-        System.out.println(Integer.toBinaryString(-2));
+        System.out.println(Integer.toBinaryString(-2)); //Kettes komplementer szerinti karakter sorozat
+        System.out.println(Integer.toString(3, 2));//Int konvert bármelyik számrendszerbe
 
 
     }
