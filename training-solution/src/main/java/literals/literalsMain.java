b9da77383ee05edbc65d10f419106690f460618b
hadikp <hp20001025@gmail.com>
2021-10-25 19:27:06 +0200

    Literálok

---
+++ training-solution/src/main/java/literals/literalsMain.java
@@ -0,0 +1,24 @@
+package literals;
+
+public class literalsMain {
+    public static void main(String[] args) {
+        System.out.println("" + 1 + 2);
+        System.out.println(1  + "" + 2);
+        System.out.println("1" + 2);
+
+        double quotient1 = 3 / 4;
+        double quotient2 = 3*1.0 / 4;
+        System.out.println(quotient1);
+        System.out.println(quotient2);
+
+        System.out.println();
+        long big = 3_244_444_444L;
+        System.out.println(big);
+        String word = "title";
+        System.out.println(word.toUpperCase());
+        System.out.println(Integer.toBinaryString(3)); //Integer konvertálása binárissá
+        System.out.println(Integer.toBinaryString(-2));
+
+
+    }
+}
