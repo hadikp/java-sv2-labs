62ad08c151245877d1d5d93c82150c87fc961e77
hadikp <hp20001025@gmail.com>
2021-10-26 14:10:31 +0200

    Számítógép kezdetek

---
+++ training-solution/src/main/java/compositionlist/computer/PersonalComputer.java
@@ -0,0 +1,9 @@
+package compositionlist.computer;
+
+import java.util.List;
+
+public class PersonalComputer {
+    private List<Hardware> hardwares;
+    private List<Software> softwares;
+    private Cpu cpu;
+}
