4493ec62e955cd5408fbfd23889241aee4a27eaa
hadikp <hp20001025@gmail.com>
2021-10-27 17:34:23 +0200

    javítás

--- training-solution/src/main/java/arrays/ArraysMain.java
+++ training-solution/src/main/java/arrays/ArraysMain.java
@@ -10,6 +10,11 @@ public class ArraysMain {
 
     public String multiplicationTableAsString(int size) {
         int[][] table = new int[size][size];
+        for (int i = 0;i < size;i++){
+            for (int j = 0;j < size;j++){
+                table[i][j]=(i + 1)*(j + 1);
+            }
+        };
         return Arrays.deepToString(table);
     }
 
