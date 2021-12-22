5163813c237a722897bf3c5692c2c2c96ccd8f5d
hadikp <hp20001025@gmail.com>
2021-12-22 20:18:01 +0100

    Lakók parkolóhelyei

---
+++ training-solution/src/main/java/isahasa/inhabitant/ParkingPlace.java
@@ -0,0 +1,18 @@
+package isahasa.inhabitant;
+
+public class ParkingPlace {
+
+    private int number;
+
+    public ParkingPlace(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
