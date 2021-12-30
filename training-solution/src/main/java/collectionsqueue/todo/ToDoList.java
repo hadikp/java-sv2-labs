b481cac4a88451b82f991496742f897da862d61b
hadikp <hp20001025@gmail.com>
2021-12-30 20:54:04 +0100

    Sürgős tennivalók

---
+++ training-solution/src/main/java/collectionsqueue/todo/ToDoList.java
@@ -0,0 +1,37 @@
+package collectionsqueue.todo;
+
+import java.util.*;
+
+public class ToDoList {
+
+    private List<ToDo> todos = new ArrayList<>();
+
+    public ToDoList(List<ToDo> todos) {
+        this.todos = todos;
+    }
+
+    public Deque<ToDo> getTodosInUrgencyOrder() {
+        Deque<ToDo> result = new ArrayDeque<>();
+        for (ToDo td: todos) {
+            if (td.isUrgent() == true) {
+                result.push(td);
+            } else {
+                result.addLast(td);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ToDo reggeli = new ToDo("reggelizés", false);
+        ToDo tizorai = new ToDo("tízórai", false);
+        ToDo ebed = new ToDo("ebédelés", true);
+        ToDo uzsonna  = new ToDo("uzsonnázás", false);
+        ToDo vacsora = new ToDo("vacsorázás", false);
+        ToDo kod = new ToDo("Java kódírás", true);
+        List<ToDo> todos = Arrays.asList(reggeli, tizorai, ebed, uzsonna, vacsora, kod);
+        ToDoList toDoList = new ToDoList(todos);
+        System.out.println(toDoList.getTodosInUrgencyOrder());
+        System.out.println(toDoList.getTodosInUrgencyOrder().pop());
+    }
+}
