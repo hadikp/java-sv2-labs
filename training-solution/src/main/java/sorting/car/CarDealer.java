d937bf2e305bc2eaf037dc1b04503fd0d1fd7824
hadikp <hp20001025@gmail.com>
2022-01-10 12:37:39 +0100

    car sale

---
+++ training-solution/src/main/java/sorting/car/CarDealer.java
@@ -0,0 +1,30 @@
+package sorting.car;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CarDealer {
+
+    private List<Car> saledCars = new ArrayList<>();
+
+    public void addCar(Car car) {
+        saledCars.add(car);
+    }
+
+    public List<Car> getCarsOrderedByYearOfConstruction() {
+        List<Car> result = new ArrayList<>(saledCars);
+        Collections.sort(result, new CarConstructionComparator());
+        return result;
+    }
+
+    public List<Car> getCarsOrderedByPrice() {
+        List<Car> result = new ArrayList<>(saledCars);
+        Collections.sort(result, new CarPriceComparator());
+        return result;
+    }
+
+    public List<Car> getSaledCars() {
+        return saledCars;
+    }
+}
